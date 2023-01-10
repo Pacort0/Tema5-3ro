3a52e8c4287ed23061f2d21bb502d00a1efb531f
Admin <Admin@DESKTOP-FNNFMRC.home>
2023-01-10 18:02:10 +0100

    segundo ejercicio hecho en casica

--- Tema5-TercerosEjercicios/src/ejercicios/Ejercicio02.java
+++ Tema5-TercerosEjercicios/src/ejercicios/Ejercicio02.java
@@ -7,13 +7,20 @@ public class Ejercicio02 {
 	public static void main(String[] args) {
 		
 		int tabla[] = new int [20];
+		int contadorImpar = 0;
+		int contadorPar = 0;
+		int num;
 		
 		for(int i = 0; i < tabla.length; i++) {
-			tabla[i] = (int)(Math.random()*100 - 0);
-		}
-		
-		for(int i = 0; i < tabla.length; i++) {
-			
+			num = (int)(Math.random()*100 - 0);
+			if(num % 2 == 0) {
+				tabla[contadorPar] = num;
+				contadorPar++;
+			}
+			else {
+				tabla[(tabla.length-1) - contadorImpar] = num;
+				contadorImpar++;	
+			}
 		}
 		
 		System.out.print(Arrays.toString(tabla));
